8b4fa56436a757204594ccf8d8557354a6b57a0a
Alan <alan.figueroa@hotmail.com>
2025-10-14 05:06:31 -0600

    feat: add Experience

---
+++ src/components/ExperienceList.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function ExperienceList({ description }) {
+  const [showAll, setShowAll] = useState(false);
+
+  return (
+    <>
+      <ul className="list-disc list-inside">
+        {(showAll ? description : description.slice(0, 3)).map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      {description.length > 3 && (
+        <button
+          className="flex items-center gap-1 text-[#2afc85] hover:text-[#15c46a] transition-colors"
+          type="button"
+          onClick={() => setShowAll(!showAll)}
+        >
+          {showAll ? "Mostrar menos" : "Mostrar más"}
+          {showAll ? (
+            <i className="fa-solid fa-arrow-up"></i>
+          ) : (
+            <i class="fa-solid fa-arrow-down"></i>
+          )}
+        </button>
+      )}
+    </>
+  );
+}
